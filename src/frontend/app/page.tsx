e33cd309eb4aa373973402f416a1147d375b6a75
Aryo Wisanggeni <13523100@std.stei.itb.ac.id>
2024-12-05 04:30:03 +0700

    feat: add function to convert mapper to postgre database

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import DragandDrop from "@/components/draganddrop";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="absolute content-center">
-      <h1 className="text-2xl">Hello World!</h1>
+    <div className="absolute inset-0 flex items-center justify-center min-h-screen">
+      {/* <h1 className="text-2xl">Hello World!</h1> */}
+      <DragandDrop/>
     </div>
   );
 }
